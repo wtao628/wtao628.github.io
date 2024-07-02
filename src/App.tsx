e8acb1a947b4b45f578d862c4dc592e9d26700f3
wtao628 <winstonpabc@hotmail.com>
2024-07-02 08:42:33 -0700

    feat: Add source code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+/**
+ * The wrapper for the web application.
+ * @returns {React.JSX.Element} The web application.
+ */
+function App(): React.JSX.Element {
+    return <p>This website is under construction!</p>
+}
+
+export default App
