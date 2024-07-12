4090f572458d366810721510caded199a2d08210
wtao628 <winstonpabc@hotmail.com>
2024-07-12 14:19:47 -0700

    fix: Simplify error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,21 +1,15 @@
-import { Text, Title } from '@mantine/core';
+import { Flex, Text, Title } from '@mantine/core';
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
 
 /**
  * An error page for when something goes wrong.
  * @returns {React.JSX.Element} An error page.
  */
 export default function ErrorPage(): React.JSX.Element {
-  const error = useRouteError();
-
   return (
-    <div id='error-page'>
+    <Flex gap='md' align='center' direction='column'>
       <Title order={1}>Oops!</Title>
-      <Title order={2}>Something went wrong...</Title>
-      <Text fs='italic'>
-        {(error as Error).message || (error as { statusText?: string }).statusText}
-      </Text>
-    </div>
+      <Text fs='italic'>Something went wrong...</Text>
+    </Flex>
   )
 }
