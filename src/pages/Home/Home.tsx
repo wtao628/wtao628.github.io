1cfb0a17a4ef977a5a7a0cb032eab2943126b160
wtao628 <winstonpabc@hotmail.com>
2024-07-23 23:25:50 -0700

    feat: Add sidebar below animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
-import { Center, Container, Flex, Grid, Paper, Text, Title } from "@mantine/core";
+import { ActionIcon, Center, Container, Flex, Grid, Group, Image, Paper, Text, Title } from "@mantine/core";
+import { IconBrandGithub, IconBrandLinkedin, IconBuilding, IconMapPin, IconSchool } from "@tabler/icons-react";
 import React from "react";
 
 import './Home.css';
@@ -18,18 +19,51 @@ export default function Home({ height }: HomeProps): React.JSX.Element {
     <div>
       <Center h={height} id='intro'>
         <Flex align='center' direction='column'>
-          <Title size='80'>Hey there!</Title>
+          <Title size={80}>Hey there!</Title>
           <Title order={1}>I'm Winston.</Title>
         </Flex>
       </Center>
-      <Container>
+      <Container h={height} pt='lg' pb='lg'>
         <Grid>
-          <Grid.Col span={4}>
+          <Grid.Col span={4.5}>
             <Paper shadow='md' radius='lg'>
-              <Text>Hi</Text>
+              <Flex align='center' direction='column'>
+                <Image radius='xl' src={null} pt='lg' />
+                <Title order={3}>Winston Tao</Title>
+                <Text fs='italic' pb='md'>Undergraduate Student</Text>
+                <Group gap='xs' pb='md'>
+                  <ActionIcon
+                    variant='white'
+                    onClick={() => window.open('https://github.com/wtao628')}
+                    >
+                    <IconBrandGithub className='icon' />
+                  </ActionIcon>
+                  <ActionIcon
+                    variant='white'
+                    onClick={() => window.open('https://ca.linkedin.com/in/winston-tao-ab3b57262')}
+                    >
+                    <IconBrandLinkedin className='icon' />
+                  </ActionIcon>
+                </Group>
+                <Flex direction='column' pb='lg'>
+                  <Group gap='xs'>
+                    <IconSchool className='icon' />
+                    <Text>University of British Columbia</Text>
+                  </Group>
+                  <Group gap='xs'>
+                    <IconBuilding className='icon' />
+                    <Text>Korotu Technology Inc.</Text>
+                  </Group>
+                  <Group gap='xs'>
+                    <IconMapPin className='icon' />
+                    <Text>Vancouver, CA</Text>
+                  </Group>
+                </Flex>
+              </Flex>
             </Paper>
           </Grid.Col>
-          <Grid.Col span={8}>2</Grid.Col>
+          <Grid.Col span={7.5}>
+          </Grid.Col>
         </Grid>
       </Container>
     </div>
